d1baf62e4ccf7c551104a2af0e56a0e4b1fea6ef
Zarah Dominguez <zarah@zdominguez.com>
2017-10-08 17:15:01 +1100

    Use a custom visibility BindingAdapter

--- app/src/main/java/io/plaidapp/util/DatabindingUtils.java
+++ app/src/main/java/io/plaidapp/util/DatabindingUtils.java
@@ -3,6 +3,8 @@ package io.plaidapp.util;
 
 import android.databinding.BindingAdapter;
 import android.databinding.ObservableBoolean;
+import android.view.View;
+
 public class DatabindingUtils {
     public static class LoadingState {
         public final ObservableBoolean isLoading = new ObservableBoolean();
@@ -11,4 +13,8 @@ public class DatabindingUtils {
         }
     }
 
+    @BindingAdapter("visibility")
+    public static void setVisibility(View view, boolean isVisible) {
+        view.setVisibility(isVisible ? View.VISIBLE : View.GONE);
+    }
 }
